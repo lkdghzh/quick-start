47fe0e85d95773736538dfd0c0fdd27515dc7e8c
lkdghzh <lkdghzh@gmail.com>
2025-04-26 02:29:23 +0800

    fix:Android 安装 27 ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.getx_demo"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
